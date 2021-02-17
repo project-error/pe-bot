3a934e1b50ca20e851af1d005cbad088f316ef50
Taso <taso@projecterror.dev>
2021-02-17 09:42:28 -0500

    tweak(env): Tweaked env injection for dev/prod

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,12 @@
-import 'dotenv/config';
+import dotenv from 'dotenv';
 import PEBot from './client/PEBot';
+import path from 'path';
+
+if (process.env.NODE_ENV === 'development') {
+  const env = dotenv.config({ path: path.resolve('.env.dev') });
+} else {
+  dotenv.config();
+}
 
 const peBot = new PEBot();
 

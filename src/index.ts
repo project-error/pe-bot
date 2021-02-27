16c32ae9d8c9e4307bc438ee1a5e57f99e0b3bae
Taso <Taso@projecterror.dev>
2021-02-26 20:35:36 -0500

    fix(bot/start): Remove useless catch resulting in unknown location log

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,8 @@
 require('./utils/dotenv');
 import 'reflect-metadata';
 import PEBot from './client/PEBot';
-import { getLogger } from './utils/logger';
 import './express/init';
-
-const tempLog = getLogger();
 
 const peBot = new PEBot();
 
-peBot.start().catch((e) => tempLog.error(e));
+peBot.start();

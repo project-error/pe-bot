ffa25bc85a5feeb4d22c8d4357c8aefb76ecaf5c
chip <chip@projecterror.dev>
2021-10-05 15:35:12 +0200

    Just make this bot toxic

--- src/commands/fun/Joke.ts
+++ src/commands/fun/Joke.ts
@@ -15,11 +15,13 @@ export default class Joke extends Command {
     });
   }
   async exec(message: Message): Promise<Message> {
-    const joke = await fetchUrl('https://v2.jokeapi.dev/joke/Miscellaneous,Dark?blacklistFlags=religious,political,racist,sexist&type=twopart');
-    const jokeEmbed = new MessageEmbed()
+    const joke = await fetchUrl('https://v2.jokeapi.dev/joke/Any?type=twopart');
+    
+    jokeEmbed = new MessageEmbed()
       .setTitle(`Funny mey mey 😂`)
       .addField(joke.setup, joke.delivery)
       .setColor('ORANGE');
+    
     return message.channel.send(jokeEmbed);
   }
 }

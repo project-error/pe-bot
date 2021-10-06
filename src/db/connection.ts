98d4f1d8d78e0504bdb47222a9f72d6082d09513
Taso <taso@projecterror.dev>
2021-10-05 20:46:29 -0400

    fix(db): dynamically set hostname from env vars

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -9,8 +9,8 @@ connectManager.create({
   entities: [Ban, Warning, Kick],
   type: 'postgres',
   database: 'pe-bot',
-  host: 'localhost',
-  port: 5432,
+  host: process.env.DB_HOSTNAME,
+  port: parseInt(process.env.DB_PORT as string) || 5432,
   username: process.env.DB_USERNAME || 'postgres',
   password: process.env.DB_PASSWORD || 'devlocal',
 });
